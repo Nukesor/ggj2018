889a849f2d6dc4c6ddd7f550bc2571c5f479604b
fabianbehrendt <fabian.behrendt95@gmail.com>
2018-01-27 19:40:11 +0100

    add collision detection

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,7 +7,6 @@ var player_radius = 4.0
 var wave_radius = 120
 var wave_origin
 
-var collision = false
 var go_scene = load("res://scenes/game_over.tscn")
 var go_node = go_scene.instance()
 
@@ -15,15 +14,16 @@ var go_node = go_scene.instance()
 func _ready():
 	wave_origin = get_node('/root/World/whale').get_pos()
 	set_process(true)
+	set_fixed_process(true)
+	
+	# Set the position of the Area2D
+	var area2D = get_node("Area2D")
+	area2D.set_pos(Vector2(wave_radius, 0))
+	area2D.connect('area_enter', self, 'collide')
 
 func _draw():
 	# Draw the player
-#	draw_circle(wave_origin + Vector2(wave_radius, 0.0), player_radius, Color(1.0, 1.0, 1.0))
 	draw_circle(Vector2(wave_radius, 0), player_radius, Color(1.0, 1.0, 1.0))
-
-	# Add the collider
-	get_node("CollisionShape2D").get_shape().set_radius(player_radius)
-
 	set_pos(wave_origin)
 
 func _process(delta):
@@ -36,9 +36,7 @@ func _process(delta):
 		elif Input.is_action_pressed("player_right"):
 			player_angle -= player_speed
 
-		#set_pos(wave_origin + Vector2(cos(player_angle), sin(player_angle)) * wave_radius)
 		set_rot(player_angle)
-	
-	if collision:
-		get_node("/root").add_child(go_node)
-		collision = false
+
+func collide(other):
+	get_node("/root").add_child(go_node)
