bcf4ac8ef07f24eb463a7ea8778f057ebaa35126
C-gotoh <shigotoh1@gmail.com>
2018-01-27 15:19:05 +0100

    integrate speed variables in waves

--- scripts/Wave.gd
+++ scripts/Wave.gd
@@ -5,31 +5,44 @@ extends Node2D
 # var b = "textvar"
 
 var player
-var circleradius
-var timer
-var finalwavedist
-var finalradius
-var waveangle
 var waveletscene
+
+var	timer = 0
+var	globaltimer = 0
+
+var	circleradius = 0
+var	finalwavedist = 0
+var	finalradius = 100
+var	wobble = 0
+var wobblespeed = 10
+var	waveangle = [45,135]
+
+var	waveletspeed = 10
+var waveletspawnrate = 0.5
 
 func _draw():
 	#draw_circle(player.get_pos(), circleradius, Color(255,255,255,1))
 	# final wave
-	draw_circle_arc(player.get_pos()+Vector2(finalwavedist,0), finalradius, waveangle[0], waveangle[1], Color(255,255,255,1))
+	draw_circle_arc(player.get_pos()+Vector2(finalwavedist,0), finalradius+wobble, waveangle[0], waveangle[1], Color(255,255,255,1))
+	draw_circle_arc(player.get_pos()+Vector2(finalwavedist,0), finalradius+wobble-0.3, waveangle[0], waveangle[1], Color(255,255,255,0.5))
+	draw_circle_arc(player.get_pos()+Vector2(finalwavedist,0), finalradius+wobble-0.5, waveangle[0], waveangle[1], Color(255,255,255,0.3))
 	pass
 
 func _process(delta):
-	update()
 	#circleradius += 0.2
-	timer += 0.2
-	if timer > 20:
+	wobble = 2*sin(wobblespeed*globaltimer)
+	print(wobble)
+	timer += delta
+	globaltimer += delta
+	if timer > 1/waveletspawnrate:
 		timer = 0
 		#var node = waveletscene.instance(finalradius)
 		var node = waveletscene.instance()
 		node.set_radius(finalradius)
 		node.set_waveangle(waveangle)
+		node.set_speed(waveletspeed)
 		add_child(node)
-
+	update()
 	pass
 
 func draw_circle_arc( center, radius, angle_from, angle_to, color ):
@@ -50,10 +63,5 @@ func _ready():
 	print("init wave")
 	player = get_node("/root/World/Player")
 	waveletscene = preload("res://scenes/Wavelet.tscn")
-	circleradius = 0
-	finalwavedist = 0
-	finalradius = 100
-	timer = 0
-	waveangle = [45,135]
 	set_process(true)
 	pass
