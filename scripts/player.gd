ead9d3b3c3cf33f07adf166986fdaa9bde3e228d
fabianbehrendt <fabian.behrendt95@gmail.com>
2018-01-27 14:32:04 +0100

    add player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,23 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var angle = 0.0
+var speed = 5.0
+var radius = 50.0
+var wave_origin = Vector2(125, 75)
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_process(true)
+
+func _process(delta):
+	if Input.is_action_pressed("player_left") or Input.is_action_pressed("player_right"):
+		var player_pos = get_pos()
+		
+		if Input.is_action_pressed("player_left"):
+			angle -= speed
+			
+		elif Input.is_action_pressed("player_right"):
+			angle += speed
+		
+		player_pos = wave_origin + Vector2(cos(deg2rad(angle)), sin(deg2rad(angle))) * radius
+		set_pos(player_pos)
+		get_node("AnimatedSprite").set_rotd(-angle)
