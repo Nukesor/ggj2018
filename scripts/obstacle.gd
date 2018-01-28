be97da0095b89b237ff3a30181cdc8a1bd509a8a
Rafael Epplee <raffomania@gmail.com>
2018-01-28 02:05:25 +0100

    algae

--- scripts/obstacle.gd
+++ scripts/obstacle.gd
@@ -3,7 +3,7 @@ extends Node2D
 export var speed = 50
 
 var start_position
-var deco_names = ['coral']
+var deco_names = ['coral', 'algae']
 
 func rand_element(arr):
 	return arr[rand_range(0, arr.size())]
@@ -14,9 +14,11 @@ func _ready():
 
     if randf() > 0.2:
         var node = load('res://scenes/obstacles/' + rand_element(deco_names) + '.tscn').instance()
-        var x = rand_range(-10, 10)
-        var y = rand_range(-10, 10)
+        var x = rand_range(-15, 15)
+        var y = rand_range(-15, 15)
         node.set_pos(Vector2(x, y))
+        var brightness = rand_range(0.8, 1.1)
+        node.set_modulate(Color(brightness, brightness, brightness))
         if randf() > 0.5:
             node.set_scale(Vector2(-1.0, 1))
         add_child(node)
