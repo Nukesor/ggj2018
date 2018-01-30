dbb316c1ac291fd64ca289ff42114754e2b6d692
Rafael Epplee <raffomania@gmail.com>
2018-01-30 16:44:07 +0100

    re-add bubbles and fish

--- scenes/deco/fish.gd
+++ scenes/deco/fish.gd
@@ -1,11 +1,7 @@
 extends AnimatedSprite
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
 	set_process(true)
-	
+
 func _process(dt):
-	translate(Vector2(-10*dt, 0))
+	translate(Vector2(-20*dt, 0))
--- scripts/deco_spawner.gd
+++ scripts/deco_spawner.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-var deco_names = ['bubble', 'fish']
+var deco_names = ['bubble', 'fish', 'bubble']
 var scenes = []
 var obstacle_script = preload('res://scripts/obstacle.gd')
 
 func _ready():
-	get_node('Timer').connect('timeout', self, 'maybe_spawn')
+	get_node('Timer').connect('timeout', self, 'timeout')
 	for name in deco_names:
 		scenes.append(load('res://scenes/deco/' + name + '.tscn'))
 
@@ -18,7 +18,12 @@ func prepare_node(scene):
 	node.add_child(scene.instance())
 	return node
 
-func maybe_spawn():
+func timeout():
+	for deco in get_children():
+		var x = deco.has_method('get_global_pos') && deco.get_global_pos().x
+		if x && (x < -100 || x > 356):
+			deco.queue_free()
+
 	if randf() > 0.3:
 		var node = prepare_node(rand_element(scenes))
 		node.set_pos(Vector2(rand_range(-20, 20), rand_range(-100, 100)))
